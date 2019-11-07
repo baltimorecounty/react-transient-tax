ab083099a3e9f2dd7f42ce7017178b8247241a68
martypowell <jpowel25@gmail.com>
2019-11-07 13:55:56 -0500

    implemented calculation function for interest

---
+++ src/common/Calculations.js
@@ -0,0 +1,15 @@
+/**
+ * Calulate the interest based on tax collected and the number of months late.
+ * @param {number} taxCollected dollar amount to base the interest off
+ * @param {number} numberOfMonthsLate number of months the user has failed to pay their tax
+ * @param {number} interest - interest rate to charge
+ */
+const CalulateInterest = (
+  taxCollected,
+  numberOfMonthsLate,
+  interest = 0.01
+) => {
+  return taxCollected * interest * numberOfMonthsLate;
+};
+
+export { CalulateInterest };
