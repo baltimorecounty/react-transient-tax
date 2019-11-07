a0c00331c4d0b5fd8f331d2291220bad053c1b52
martypowell <jpowel25@gmail.com>
2019-11-07 14:06:15 -0500

    cleaned up comment and converted to lambda function

--- src/common/Calculations.js
+++ src/common/Calculations.js
@@ -2,14 +2,9 @@
  * Calulate the interest based on tax collected and the number of months late.
  * @param {number} taxCollected dollar amount to base the interest off
  * @param {number} numberOfMonthsLate number of months the user has failed to pay their tax
- * @param {number} interest - interest rate to charge
+ * @param {number} interest - interest rate to charge as a decimal
  */
-const CalulateInterest = (
-  taxCollected,
-  numberOfMonthsLate,
-  interest = 0.01
-) => {
-  return taxCollected * interest * numberOfMonthsLate;
-};
+const CalulateInterest = (taxCollected, numberOfMonthsLate, interest = 0.01) =>
+  taxCollected * interest * numberOfMonthsLate;
 
 export { CalulateInterest };
