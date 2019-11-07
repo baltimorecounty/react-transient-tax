a7de4840d371b2d066b629f8f5e466f9c4e9ff0c
martypowell <jpowel25@gmail.com>
2019-11-07 14:14:22 -0500

    created a contstants file and moved out percetages

--- src/common/Calculations.js
+++ src/common/Calculations.js
@@ -1,18 +1,25 @@
+import { RatesAndFees } from "./Constants";
+
 /**
  * Calulate the interest based on tax collected and the number of months late.
  * @param {number} taxCollected dollar amount to base the interest off
  * @param {number} numberOfMonthsLate number of months the user has failed to pay their tax
  * @param {number} interest - interest rate to charge as a decimal
  */
-const CalulateInterest = (taxCollected, numberOfMonthsLate, interest = 0.01) =>
-  taxCollected * interest * numberOfMonthsLate;
+const CalulateInterest = (
+  taxCollected,
+  numberOfMonthsLate,
+  interest = RatesAndFees.InterestRate
+) => taxCollected * interest * numberOfMonthsLate;
 
 /**
  * Calulate the penalty fee based on tax collected.
  * @param {number} taxCollected dollar amount to base the penalty off of
- * @param {number} penaltyFee penalty fee percentage as a decimal
+ * @param {number} penaltyRate penalty fee percentage as a decimal
  */
-const CalculatePenalty = (taxCollected, penaltyFee = 0.1) =>
-  taxCollected * penaltyFee;
+const CalculatePenalty = (
+  taxCollected,
+  penaltyRate = RatesAndFees.PenaltyRate
+) => taxCollected * penaltyRate;
 
 export { CalulateInterest, CalculatePenalty };
