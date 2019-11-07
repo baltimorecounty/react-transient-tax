2439875076f790c2527f44b51b85c403338baeb7
martypowell <jpowel25@gmail.com>
2019-11-07 15:07:04 -0500

    intergrated react-date-pciker

--- src/components/ReturnInterval.jsx
+++ src/components/ReturnInterval.jsx
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const ReturnInterval = props => {
+  const [months, setMonths] = useState({});
   const { intervalType } = props;
+  const isMonthly = intervalType === "monthly";
+  const monthsToSelect = new Array(isMonthly ? 1 : 3).fill(
+    null
+  ); /** 3 months per quarter */
+
+  const handleDateChange = (date, monthIndex) => {
+    const newMonths = { ...months };
+    newMonths[monthIndex] = date;
+    setMonths(newMonths);
+  };
+
   return (
     <div className="">
-      <label htmlFor="">
-        Month{intervalType === "monthly" ? "" : "s"} for Return
-      </label>
+      <label htmlFor="">Month{isMonthly ? "" : "s"} for Return</label>
+      {monthsToSelect.map((month, monthIndex) => {
+        return (
+          <DatePicker
+            key={monthIndex}
+            selected={months[monthIndex]}
+            onChange={date => handleDateChange(date, monthIndex)}
+            selectsStart
+            // startDate={startDate}
+            // endDate={endDate}
+            dateFormat="MM/yyyy"
+            showMonthYearPicker
+          />
+        );
+      })}
     </div>
   );
 };
