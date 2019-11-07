e9dc139f52c4d91314c6f07dcfa0ef464e8a9f5c
martypowell <jpowel25@gmail.com>
2019-11-07 15:40:39 -0500

    added test to support due date

---
+++ src/common/Dates.test.js
@@ -0,0 +1,10 @@
+import { GetDueDate } from "./Dates";
+
+describe("Get Due Date", () => {
+  test("should return February 28th for a January return", () => {
+    const returnDate = new Date("January 1, 2019");
+    const actual = GetDueDate(returnDate);
+    const expected = new Date("February 28, 2019");
+    expect(actual).toEqual(expected);
+  });
+});
