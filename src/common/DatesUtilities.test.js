571f813c623b32c5f46bec7b40e3528b00305056
martypowell <jpowel25@gmail.com>
2019-11-08 07:54:34 -0500

    added tests for leap year and over year

--- src/common/DatesUtilities.test.js
+++ src/common/DatesUtilities.test.js
@@ -1,8 +1,18 @@
 import { GetDueDate } from "./DatesUtilities";
 
 describe("Get Due Date", () => {
-  test("should return February 28th for a January return", () => {
+  test("should return the proper due date on a normal year", () => {
     const actual = GetDueDate(new Date("January 1, 2019"));
     expect(actual).toEqual("February 28, 2019");
+  });
+
+  test("should return the proper due date on a leap year", () => {
+    const actual = GetDueDate(new Date("January 1, 2020"));
+    expect(actual).toEqual("February 29, 2020");
+  });
+
+  test("should return the proper due date, when the due date happens to be in the following year", () => {
+    const actual = GetDueDate(new Date("Decemeber 15, 2019"));
+    expect(actual).toEqual("January 31, 2020");
   });
 });
