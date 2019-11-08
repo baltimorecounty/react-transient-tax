7affc542e0fd512907ea05ef5a86782b9fae2832
martypowell <jpowel25@gmail.com>
2019-11-08 07:50:56 -0500

    added test to verify due date funciton

--- src/common/DatesUtilities.test.js
+++ src/common/DatesUtilities.test.js
@@ -2,9 +2,7 @@ import { GetDueDate } from "./DatesUtilities";
 
 describe("Get Due Date", () => {
   test("should return February 28th for a January return", () => {
-    const returnDate = new Date("January 1, 2019");
-    const actual = GetDueDate(returnDate);
-    const expected = new Date("February 28, 2019");
-    expect(actual).toEqual(expected);
+    const actual = GetDueDate(new Date("January 1, 2019"));
+    expect(actual).toEqual("February 28, 2019");
   });
 });
