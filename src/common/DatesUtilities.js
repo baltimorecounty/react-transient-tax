60d93dc73a15e988ac33af3f8ed5b677ee0149fd
martypowell <jpowel25@gmail.com>
2019-11-08 07:51:09 -0500

    implemented due date function using date-fns

--- src/common/DatesUtilities.js
+++ src/common/DatesUtilities.js
@@ -1,13 +1,15 @@
-import { addMonths, endOfMonth } from "date-fns";
+import { addMonths, endOfMonth, format } from "date-fns";
+const dateFormat = "MMMM d, yyyy";
 
 /**
  * Get the due date based on a given date.
  * The due date is the end of the next month.
- * @param {date} date
+ * @param {date} date date to base the due date calculation string
+ * @returns {string} friendly version of the due date
  */
 const GetDueDate = date => {
   const nextMonth = addMonths(date, 1);
-  return endOfMonth(nextMonth);
+  return format(endOfMonth(nextMonth), dateFormat);
 };
 
 export { GetDueDate };
