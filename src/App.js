9b6e9e39a63469a4086b5c60b269ca8ef0841355
martypowell <jpowel25@gmail.com>
2019-11-12 08:07:52 -0500

    added routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
 import "./App.scss";
 import TransientTaxForm from "./components/TransientTaxForm";
 
 function App() {
   return (
     <div className="tt_app">
-      <TransientTaxForm />
+      <Router>
+        <Route exact path="/" component={TransientTaxForm} />
+      </Router>
     </div>
   );
 }
