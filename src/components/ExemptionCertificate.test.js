a86cef5b23a0c445b03a80b8583141ac24056aa1
martypowell <jpowel25@gmail.com>
2019-11-12 15:13:18 -0500

    removed render test

--- src/components/ExemptionCertificate.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import ExemptionCertificate from "./ExemptionCertificate";
-
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<ExemptionCertificate />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
