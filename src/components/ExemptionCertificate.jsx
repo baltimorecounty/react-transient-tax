5f312553654b0d1501210a3ad1f1af20a289fdf8
martypowell <jpowel25@gmail.com>
2019-11-12 10:29:58 -0500

    simpolified import

--- src/components/ExemptionCertificate.jsx
+++ src/components/ExemptionCertificate.jsx
@@ -1,20 +1,19 @@
 import React from "react";
+import ExemptionSelector from "./ExemptionSelector";
 
-const ExemptionCertificate = props => {
-  return (
-    <div className="tt_form-section">
-      <p>
-        I certify that the occupancy of the facilities above have been (or will
-        be) furnished for the exclusive use of and will be paid by or from the
-        funds of:
-      </p>
-      <p>
-        That all of the qualifications explained on this form have been met so
-        as to exempt this occupancy from the tax imposed by County Code
-        11-4-401.
-      </p>
-    </div>
-  );
-};
+const ExemptionCertificate = props => (
+  <div className="tt_form-section">
+    <p>
+      I certify that the occupancy of the facilities above have been (or will
+      be) furnished for the exclusive use of and will be paid by or from the
+      funds of:
+    </p>
+    <ExemptionSelector />
+    <p>
+      That all of the qualifications explained on this form have been met so as
+      to exempt this occupancy from the tax imposed by County Code 11-4-401.
+    </p>
+  </div>
+);
 
 export default ExemptionCertificate;
