07a8144b04a44a04418cd3728068b63ed0295361
martypowell <jpowel25@gmail.com>
2019-11-12 15:00:44 -0500

    added placeholder for data, to fake an id

---
+++ src/services/ApiService.js
@@ -0,0 +1,9 @@
+let exemptionId = 0;
+
+const SaveExemption = exemption => {
+  exemptionId += 1; /** fake generating an id so we don't lose track of exemptions, this will be replaced with service logic */
+
+  return { ...exemption, id: exemptionId };
+};
+
+export { SaveExemption };
