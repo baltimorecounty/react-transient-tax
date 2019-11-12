11167d8c318a6e900bfdcfb9aa42aa1929037c98
martypowell <jpowel25@gmail.com>
2019-11-12 08:56:05 -0500

    added required text to exemption cert

--- src/components/ExemptionCertificate.jsx
+++ src/components/ExemptionCertificate.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 const ExemptionCertificate = props => {
-  return <p>Cert goes here</p>;
+  return (
+    <div className="tt_form-section">
+      <p>
+        I certify that the occupancy of the facilities above have been (or will
+        be) furnished for the exclusive use of and will be paid by or from the
+        funds of:
+      </p>
+      <p>
+        That all of the qualifications explained on this form have been met so
+        as to exempt this occupancy from the tax imposed by County Code
+        11-4-401.
+      </p>
+    </div>
+  );
 };
 
 export default ExemptionCertificate;
