06427a814a432ed19d3883dcb71d4bc62b976c75
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2019-11-13 15:43:38 -0500

    fixed naming in test file

--- src/common/Caculations.test.js
+++ src/common/Caculations.test.js
@@ -1,13 +1,13 @@
-import { CalulateInterest, CalculatePenalty } from "./Calculations";
+import { CalculateInterest, CalculatePenalty } from "./Calculations";
 
 describe("Interest Calculation", () => {
   test("should return the proper calculation when passed valid data based on the default interest rate of 1", () => {
-    const actual = CalulateInterest(100, 4);
+    const actual = CalculateInterest(100, 4);
     expect(actual).toEqual(4);
   });
 
   test("should return zero if the payment is still considered on time", () => {
-    const actual = CalulateInterest(100, 0);
+    const actual = CalculateInterest(100, 0);
     expect(actual).toEqual(0);
   });
 });
