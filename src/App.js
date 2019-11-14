3d8cb879a96e72746dfb8e1637a64472adf42208
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2019-11-14 14:10:13 -0500

    Initial creation

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { HashRouter as Router, Route } from "react-router-dom";
 import "react-datepicker/dist/react-datepicker.css";
 import "./App.scss";
 import TransientTaxForm from "./components/forms/TransientTaxForm";
+import ConfirmationForm from "./components/forms/ConfirmationForm";
 
 function App() {
   return (
     <div className="tt_app">
       <Router>
         <Route exact path="/" component={TransientTaxForm} />
+        <Route exact path="/ConfirmationForm" component={ConfirmationForm} />
       </Router>
     </div>
   );
