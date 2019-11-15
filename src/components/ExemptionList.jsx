d51e1e932475e9d14ed8112963e11afa0c7f11ec
martypowell <jpowel25@gmail.com>
2019-11-15 10:57:06 -0500

    simplified

--- src/components/ExemptionList.jsx
+++ src/components/ExemptionList.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import ExemptionListItem from "./ExemptionListItem";
 
-const ExemptionsList = props => {
-  const { exemptions = [], ...rest } = props;
-
-  return (
-    <ul className="tt_exemptions">
-      <h3>Exemptions</h3>
-      {exemptions.map(exemption => (
-        <ExemptionListItem key={exemption.id} exemption={exemption} {...rest} />
-      ))}
-    </ul>
-  );
-};
+const ExemptionsList = ({ exemptions = [], ...rest }) => (
+  <ul className="tt_exemptions">
+    <h3>Exemptions</h3>
+    {exemptions.map(exemption => (
+      <ExemptionListItem key={exemption.id} exemption={exemption} {...rest} />
+    ))}
+  </ul>
+);
 
 export default ExemptionsList;
