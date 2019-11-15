2def89479af7fc2bf4d3e59d276f31e0c5a6cba6
martypowell <jpowel25@gmail.com>
2019-11-15 10:35:39 -0500

    only add new id if id doesn't already exsit

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,7 +1,11 @@
 let exemptionId = 0;
 
 const SaveExemption = exemption => {
-  exemptionId += 1; /** fake generating an id so we don't lose track of exemptions, this will be replaced with service logic */
+  const { id } = exemption;
+  exemptionId =
+    id ||
+    exemptionId +
+      1; /** fake generating an id so we don't lose track of exemptions, this will be replaced with service logic */
 
   return { ...exemption, id: exemptionId };
 };
