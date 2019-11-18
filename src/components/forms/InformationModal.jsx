6db42581f3a6b854e6a7ef81f933346b7d22452f
Sanjaya Gurung <sgurung@baltimorecountymd.gov>
2019-11-18 11:27:32 -0500

    moved onclick open and close to function

--- src/components/forms/InformationModal.jsx
+++ src/components/forms/InformationModal.jsx
@@ -6,14 +6,21 @@ const InformationModal = props => {
   const [modalShow, setModalShow] = useState(false);
   const { title, content } = props;
 
+  const showModal =() =>{
+    setModalShow(true)
+  };
+  const hideModal =() =>{
+    setModalShow(false)
+  };
+
   return (
     <React.Fragment>
-      <Button type="button" color="link" onClick={() => setModalShow(true)}>
+      <Button type="button" color="link" onClick={showModal}>
         Why do I need to do this?
       </Button>
       <ModalDisplay
         show={modalShow}
-        onHide={() => setModalShow(false)}
+        onHide={hideModal}
         title={title}
         content={content}
       />
