f66de042af524b9870b507e8751d807a5b14a242
martypowell <jpowel25@gmail.com>
2019-11-19 12:26:28 -0500

    add classname from props in addiotn to component classnames

--- src/components/PaymentField.js
+++ src/components/PaymentField.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import classnames from "classnames";
 import { Field } from "formik";
 import CurrencyInput from "react-currency-input";
 
@@ -10,6 +11,7 @@ const CustomInputComponent = ({
 }) => {
   const { name } = field;
   const {
+    className,
     isNegativeValue,
     monthsToReport = {},
     label,
@@ -17,6 +19,7 @@ const CustomInputComponent = ({
   } = props;
   const { setFieldValue, touched, errors } = form;
   const [values, setValues] = useState({});
+  const cssClasses = classnames("tt_form-group flex-end total", className);
 
   const handleChange = (valueAsNumber, monthIndex) => {
     setValues({ ...values, ...{ [monthIndex]: valueAsNumber } });
@@ -27,7 +30,7 @@ const CustomInputComponent = ({
   }, [name, values, setFieldValue]);
 
   return (
-    <div className="tt_form-group flex-end">
+    <div className={cssClasses}>
       <label>{label}</label>
       <div className="tt_currency-pickers">
         {monthsToReport &&
