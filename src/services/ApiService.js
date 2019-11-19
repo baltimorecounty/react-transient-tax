16565695b7b7e97f9eb303c917a16db1e89ca579
martypowell <jpowel25@gmail.com>
2019-11-19 09:46:42 -0500

    refactoreed lookup functions

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -5,12 +5,28 @@ let exemptionId = 0;
 const apiBaseUrl = "//localhost:54727/api/transientTax";
 
 /**
+ * Get a lookup value for a given endpoint
+ * @param {string} endpoint name of endpoint for lookup values
+ */
+const GetLookupItem = endpoint =>
+  axios
+    .get(`${apiBaseUrl}/${endpoint}`)
+    .then(({ status, data }) => (status === 200 ? data : []))
+    .catch(
+      error =>
+        console.error(`Something went wrong looking up values: ${error}`) ||
+        error
+    );
+
+/**
  * Get Exemption Types
  */
-const GetExemptionTypes = () =>
-  axios
-    .get(`${apiBaseUrl}/exemptionTypes`)
-    .then(({ status, data }) => (status === 200 ? data : []));
+const GetExemptionTypes = () => GetLookupItem("exemptionTypes");
+
+/**
+ * Get Filing Types
+ */
+const GetFilingTypes = () => GetLookupItem("filingTypes");
 
 const SaveExemption = exemption => {
   const { id } = exemption;
@@ -22,4 +38,4 @@ const SaveExemption = exemption => {
   return { ...exemption, id: exemptionId };
 };
 
-export { GetExemptionTypes, SaveExemption };
+export { GetExemptionTypes, GetFilingTypes, SaveExemption };
