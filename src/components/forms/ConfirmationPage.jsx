599c6b05a64bfab37939f4927b192919148e2f51
martypowell <jpowel25@gmail.com>
2019-11-22 12:45:36 -0500

    remove labels to make it easier to follow the content

--- src/components/forms/ConfirmationPage.jsx
+++ src/components/forms/ConfirmationPage.jsx
@@ -30,16 +30,6 @@ const ConfirmationForm = props => {
     date.setMonth(date.getMonth() + 1)
   ).toString();
 
-  const labels = {
-    ConfirmationHeader: "Your Baltimore County Transient Occupancy Tax Return",
-    ConfirmationSubHeader: "Transient Tax Return Submitted",
-    ConfirmationBody: `You have successfully completed the Baltimore County Transient Occupancy Tax Return. Your confirmation number for this return is ${confirmationNumber}.`,
-    ConfirmationSubBody:
-      "Please present this number to the appropriate Budget and Finance Official when making inquiries in regards to your Transient Occupancy Tax Return.",
-    ConfirmationNextPayment: `You have signed up for ${ReturnTypeDescription} payments; the due date for your next payment is ${newDueDate}`,
-    ConfirmationTaxDetailsHeader: `${confirmationNumber} Transient Occupancy Tax Return Details:`
-  };
-
   const ConfirmationTableValues = [
     { id: 1, key: "Month of Return", value: monthSubmitted },
     { id: 2, key: "Occupancy Tax Collected", value: monthlyOccupancy },
@@ -55,19 +45,30 @@ const ConfirmationForm = props => {
       ) : (
         <div className="tt_form-section">
           <h1>
-            <span>{labels.ConfirmationHeader}</span>
+            <span>Your Baltimore County Transient Occupancy Tax Return</span>
           </h1>
           <i>
             <span className="">{DateSubmitted}</span>
           </i>
-          <h2>{labels.ConfirmationSubHeader}</h2>
-          <p> {labels.ConfirmationBody}</p>
-          <p>{labels.ConfirmationSubBody}</p>
+          <h2>Transient Tax Return Submitted</h2>
           <p>
-            <em>{labels.ConfirmationNextPayment}</em>
+            You have successfully completed the Baltimore County Transient
+            Occupancy Tax Return. Your confirmation number for this return is{" "}
+            {confirmationNumber}.
+          </p>
+          <p>
+            Please present this number to the appropriate Budget and Finance
+            Official when making inquiries in regards to your Transient
+            Occupancy Tax Return.
+          </p>
+          <p>
+            <em>
+              You have signed up for {ReturnTypeDescription} payments; the due
+              date for your next payment is {newDueDate}
+            </em>
           </p>
           <ConfirmationTable
-            TaxDetailsHeader={labels.ConfirmationTaxDetailsHeader}
+            TaxDetailsHeader={"Transient Occupancy Tax Return Details:"}
             ConfirmationTableValues={ConfirmationTableValues}
             DueDate={dueDate}
             ReturnType={ReturnTypeDescription}
