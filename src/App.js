baf21dc6daf990cda7eafb220ac77e3c84a991ae
Marty Powell <jpowel25@gmail.com>
2019-11-24 05:53:55 -0500

    created pages folder

--- src/App.js
+++ src/App.js
@@ -2,9 +2,8 @@ import React from "react";
 import { HashRouter as Router, Route } from "react-router-dom";
 import "react-datepicker/dist/react-datepicker.css";
 import "./App.scss";
-import TransientTaxForm from "./components/forms/TransientTaxForm";
+import TransientTaxForm from "./pages/TransientTaxForm";
 import ConfirmationPage from "./pages/ConfirmationPage";
-import { ConstantsProvider } from "./context/ConstantsContext";
 import { Config } from "@baltimorecounty/javascript-utilities";
 const { setConfig } = Config;
 
@@ -28,16 +27,14 @@ setConfig(configValues);
 function App() {
   return (
     <div className="tt_app">
-      <ConstantsProvider>
-        <Router>
-          <Route exact path="/" component={TransientTaxForm} />
-          <Route
-            exact
-            path="/ConfirmationPage/:confirmationNumber"
-            component={ConfirmationPage}
-          />
-        </Router>
-      </ConstantsProvider>
+      <Router>
+        <Route exact path="/" component={TransientTaxForm} />
+        <Route
+          exact
+          path="/ConfirmationPage/:confirmationNumber"
+          component={ConfirmationPage}
+        />
+      </Router>
     </div>
   );
 }
