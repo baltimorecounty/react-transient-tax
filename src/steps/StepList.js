08fd22cae2f670a3bfd7f137a057309366891575
martypowell <jpowel25@gmail.com>
2019-12-04 15:08:19 -0500

    added steplist ot manage form steps

---
+++ src/steps/StepList.js
@@ -0,0 +1,43 @@
+const generateStepNumber = steps =>
+  steps.map((step, stepIndex) => {
+    step.stepNumber = stepIndex + 1; // zero-based index
+    return step;
+  });
+
+/**
+ * A list of forms
+ */
+class StepList {
+  constructor(steps) {
+    const sequencedSteps = generateStepNumber(steps);
+    this.originalSteps = sequencedSteps;
+    this.steps = sequencedSteps;
+  }
+
+  /**
+   *
+   * @param {Step} step
+   * @param {string} insertAfterId unique id of step that you wish to insert the step after
+   */
+  addStep(step, insertAfterId) {
+    const startIndex = this.steps.findIndex(({ id }) => id === insertAfterId);
+    const newSteps = [...this.steps];
+    newSteps.splice(startIndex + 1, 0, step); // + 1
+    this.steps = generateStepNumber(newSteps);
+  }
+
+  /**
+   * Remove a step based on given unique id
+   * @param {string} idToRemove unique id to identify a certain step
+   */
+  removeStep(idToRemove) {
+    const filteredSteps = this.steps.filter(({ id }) => id !== idToRemove);
+    this.steps = generateStepNumber(filteredSteps);
+  }
+
+  reset() {
+    this.steps = this.originalSteps;
+  }
+}
+
+export default StepList;
