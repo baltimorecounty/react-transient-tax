56fee15aa7a82d8387d52986a280e87dc76fd120
martypowell <jpowel25@gmail.com>
2019-12-04 10:06:34 -0500

    adjusted form to show totals

--- src/components/forms/Form3.jsx
+++ src/components/forms/Form3.jsx
@@ -5,6 +5,7 @@ import TransientTaxTabs from "../../components/TransientTaxTabs";
 import PaymentField from "../PaymentField";
 import PaymentTotal from "../PaymentTotal";
 import { HasAtLeast1Exemption } from "../../common/ExemptionUtilities";
+import { CalculateTotalsPerMonths } from "../../common/Calculations";
 import * as Yup from "yup";
 
 const Form3 = props => {
@@ -18,25 +19,7 @@ const Form3 = props => {
     formik
   } = props;
 
-  const {
-    monthsToReport,
-    grossOccupancy,
-    governmentOnBusiness,
-    roomRentalCollectionFromNonTransients
-  } = formik.values;
-
-  const [totalExemptions, setTotalExemptions] = useState();
-  const [netRoomRentalCollections, setNetRoomRentalCollections] = useState();
-
-  useEffect(() => {
-    setTotalExemptions(
-      governmentOnBusiness + roomRentalCollectionFromNonTransients
-    );
-  }, [governmentOnBusiness, roomRentalCollectionFromNonTransients]);
-
-  useEffect(() => {
-    setNetRoomRentalCollections(grossOccupancy + totalExemptions);
-  }, [grossOccupancy, totalExemptions]);
+  const { monthsToReport = {} } = formik.values;
 
   return (
     <Formik
@@ -66,54 +49,72 @@ const Form3 = props => {
         )
       })}
     >
-      {props => (
-        <Form>
-          <TransientTaxTabs tabs={tabs} activeStep={isActiveStep} />
-          <h2>{label}</h2>
-          <div className="form-1">
-            <React.Fragment>
-              <div className="tt_form-section">
-                <PaymentField
-                  name="grossOccupancy"
-                  label={Labels.GrossOccupancy}
-                  monthsToReport={monthsToReport}
-                />
-              </div>
-            </React.Fragment>
-          </div>
-          <div className="tt_form-section">
-            <h3>{Labels.ExemptionTitle} (if applicable)</h3>
-            <PaymentField
-              isNegativeValue={true}
-              name="roomRentalCollectionFromNonTransients"
-              label={Labels.ExemptionOption1}
-              monthsToReport={monthsToReport}
-              className="tt_subtotal-item"
-            />
-            <PaymentField
-              isNegativeValue={true}
-              name="governmentOnBusiness"
-              label={Labels.ExemptionOption2}
-              monthsToReport={monthsToReport}
-              className="tt_subtotal-item"
-            />
-            <PaymentTotal
-              name="exemptionTotal"
-              totals={totalExemptions}
-              label={Labels.ExemptionTotal}
-              className="tt_subtotal"
-            />
-            <PaymentTotal
-              name="netRoomRentalTotal"
-              totals={netRoomRentalCollections}
-              label={Labels.NetRoomRentalLabel}
-              className="tt_section-total"
-            />
-          </div>
-          {prevButton}
-          {nextButton}
-        </Form>
-      )}
+      {props => {
+        const {
+          grossOccupancy,
+          governmentOnBusiness,
+          roomRentalCollectionFromNonTransients
+        } = props.values;
+
+        const totalExemptions = CalculateTotalsPerMonths(
+          [governmentOnBusiness, roomRentalCollectionFromNonTransients],
+          monthsToReport
+        );
+
+        const totalNetRoomRentals = CalculateTotalsPerMonths(
+          [totalExemptions, grossOccupancy],
+          monthsToReport
+        );
+
+        return (
+          <Form>
+            <TransientTaxTabs tabs={tabs} activeStep={isActiveStep} />
+            <h2>{label}</h2>
+            <div className="form-1">
+              <React.Fragment>
+                <div className="tt_form-section">
+                  <PaymentField
+                    name="grossOccupancy"
+                    label={Labels.GrossOccupancy}
+                    monthsToReport={monthsToReport}
+                  />
+                </div>
+              </React.Fragment>
+            </div>
+            <div className="tt_form-section">
+              <h3>{Labels.ExemptionTitle} (if applicable)</h3>
+              <PaymentField
+                isNegativeValue={true}
+                name="roomRentalCollectionFromNonTransients"
+                label={Labels.ExemptionOption1}
+                monthsToReport={monthsToReport}
+                className="tt_subtotal-item"
+              />
+              <PaymentField
+                isNegativeValue={true}
+                name="governmentOnBusiness"
+                label={Labels.ExemptionOption2}
+                monthsToReport={monthsToReport}
+                className="tt_subtotal-item"
+              />
+              <PaymentTotal
+                name="exemptionTotal"
+                totals={totalExemptions}
+                label={Labels.ExemptionTotal}
+                className="tt_subtotal"
+              />
+              <PaymentTotal
+                name="netRoomRentalTotal"
+                totals={totalNetRoomRentals}
+                label={Labels.NetRoomRentalLabel}
+                className="tt_section-total"
+              />
+            </div>
+            {prevButton}
+            {nextButton}
+          </Form>
+        );
+      }}
     </Formik>
   );
 };
