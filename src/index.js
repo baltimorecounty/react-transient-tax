429378d23f223228467094bedeb50028bf965b07
martypowell <jpowel25@gmail.com>
2019-12-04 15:07:23 -0500

    converted to use step list

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
+import { Config } from "@baltimorecounty/javascript-utilities";
 import React from "react";
+import "react-datepicker/dist/react-datepicker.css";
 import ReactDOM from "react-dom";
+import "./App.scss";
+import { format } from "date-fns";
 import BasicInformationForm1 from "./components/forms/BasicInformation-Form1";
-import PaymentOptionsForm2 from "./components/forms/PaymentOptions-Form2";
-import MonthlyPaymentForm3 from "./components/forms/MonthlyPayment-Form3";
 import ExemptionCertificateForm4 from "./components/forms/ExemptionCertificate-Form4";
 import IdentificationForm5 from "./components/forms/Identification-Form5";
-import ReviewForm6 from "./components/forms/Review-Form6";
-import { Config } from "@baltimorecounty/javascript-utilities";
-import "react-datepicker/dist/react-datepicker.css";
-import "./App.scss";
+import MonthlyPaymentForm3 from "./components/forms/MonthlyPayment-Form3";
 import MultiPageForm from "./components/forms/MultiPageForm";
+import PaymentOptionsForm2 from "./components/forms/PaymentOptions-Form2";
+import ReviewForm6 from "./components/forms/Review-Form6";
 import "./index.scss";
 import * as serviceWorker from "./serviceWorker";
+import Step from "./steps/Step";
+import StepList from "./steps/StepList";
 
 const { setConfig } = Config;
 
@@ -32,58 +35,61 @@ const configValues = {
 
 setConfig(configValues);
 
+const onPaymentSelectionSubmission = (stepList, { monthsToReport }) => {
+  stepList.reset();
+  let stepToInsertAfter = "payment-selection";
+  Object.keys(monthsToReport).forEach(monthKey => {
+    const date = monthsToReport[monthKey];
+    const friendlyDate = format(date, "MMMM yyyy");
+    const id = `payment-form-${friendlyDate.replace(/\s/g, "")}`;
+    const step = new Step({
+      id,
+      label: `${friendlyDate} Tax Return`,
+      component: <MonthlyPaymentForm3 />
+    });
+
+    stepList.addStep(step, stepToInsertAfter);
+    stepToInsertAfter = id;
+  });
+};
+
 const steps = [
-  {
-    stepNumber: 1,
+  new Step({
+    id: "basic-information",
     label: "Step 1 - Business Demographics",
     component: <BasicInformationForm1 />
-  },
-  {
-    stepNumber: 2,
+  }),
+  new Step({
+    id: "payment-selection",
     label: "Step 2 - Payment Selection",
-    component: <PaymentOptionsForm2 />
-  },
-  {
-    stepNumber: 3,
-    label: "Step 3 - Month",
-    component: <MonthlyPaymentForm3 />,
-    isHidden: true
-  },
-  {
-    stepNumber: 4,
-    label: "Step 4 - Month",
-    component: <MonthlyPaymentForm3 />,
-    isHidden: true
-  },
-  {
-    stepNumber: 5,
-    label: "Step 5 - Month",
-    component: <MonthlyPaymentForm3 />,
-    isHidden: true
-  },
-  {
-    stepNumber: 6,
+    component: <PaymentOptionsForm2 />,
+    onFormSubmission: onPaymentSelectionSubmission
+  }),
+  new Step({
+    id: "exemption-cert",
     label: "Step 6 - Exemption Certificates ",
     component: <ExemptionCertificateForm4 />,
     isHidden: true
-  },
-  {
-    stepNumber: 7,
+  }),
+  new Step({
+    id: "identification",
     label: "Step 7 - Identification",
     component: <IdentificationForm5 />
-  },
-  {
-    stepNumber: 8,
+  }),
+  new Step({
+    id: "review",
     label: "Step 8 - Review",
     component: <ReviewForm6 />,
     isForm: false
-  }
+  })
 ];
+
+const stepList = new StepList(steps);
 
 function App() {
   return (
     <div className="App">
-      <MultiPageForm steps={steps} />
+      <MultiPageForm stepList={stepList} />
     </div>
   );
 }
