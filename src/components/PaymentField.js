cd06268078327490cd77c83b2c3c4f7629545148
martypowell <jpowel25@gmail.com>
2019-12-05 10:57:07 -0500

    removed unused import

--- src/components/PaymentField.js
+++ src/components/PaymentField.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import classnames from "classnames";
 import { Field, connect } from "formik";
