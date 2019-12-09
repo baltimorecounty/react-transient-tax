1b8ad0ea9bcd1aecac86f32b9d7bc0594ac5ddd3
martypowell <jpowel25@gmail.com>
2019-12-09 14:46:57 -0500

    fixed routing

--- src/components/forms/PaymentOptions-Form2.jsx
+++ src/components/forms/PaymentOptions-Form2.jsx
@@ -16,7 +16,8 @@ const PaymentOptionsForm2 = props => {
     tabs,
     isActiveStep,
     monthsToReport,
-    label
+    label,
+    history
   } = props;
   const [filingTypes, setFilingTypes] = useState([]);
   const [paymentInterval, setPaymentInterval] = useState();
@@ -28,7 +29,7 @@ const PaymentOptionsForm2 = props => {
           setFilingTypes(filingTypes);
         })
         .catch(error => {
-          props.history.push(ErrorPath(error), { ...error });
+          history.push(ErrorPath(error), { ...error });
         });
     }
   }, [filingTypes, props]);
