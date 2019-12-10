2dff800dde03a4d5430ab48db88eec943f521ebf
tmccoy529 <40430541+tmccoy529@users.noreply.github.com>
2019-12-10 09:08:28 -0500

    Fixed endpiont

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -45,7 +45,7 @@ const GetTransientTaxReturn = confirmationNumber =>
 const GetAddresses = location =>
   axios
 
-    .get(`${getValue("gisApiRoot")}/gis/addressLookup${location}`)
+    .get(`${getValue("gisApiRoot")}/gis/addressLookup/${location}`)
     .then(({ status, data }) => (status === 200 ? data : []));
 
 const SaveExemption = exemption => {
