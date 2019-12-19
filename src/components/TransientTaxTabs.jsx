839f74d0036fb7750d4fc572105797e81daf931b
martypowell <jpowel25@gmail.com>
2019-12-19 15:22:39 -0500

    added abilty to use a parent tab

--- src/components/TransientTaxTabs.jsx
+++ src/components/TransientTaxTabs.jsx
@@ -1,21 +1,35 @@
 import React from "react";
 
 const TransientTaxTabs = props => {
-  const { tabs, isActiveStep, activeStep } = props;
+  const { panelGroups = [], tabs, activeStep } = props;
+  const hasPanelGroups = panelGroups.length > 0;
+  const itemsToMap = hasPanelGroups ? panelGroups : tabs;
+
+  const getStepByPanelGroup = activeStepId =>
+    tabs.find(step => step.stepNumber === activeStepId);
+
   return (
     <div className="bc-citysourced-reporter">
       <ol className="bc-citysourced-reporter-steps">
-        {tabs.map(step => (
-          <li
-            key={step.id}
-            style={{ display: step.isHidden ? "none" : "block" }}
-            className={
-              step.stepNumber === activeStep && isActiveStep ? "highlight" : ""
-            }
-          >
-            {step.label}
-          </li>
-        ))}
+        {itemsToMap.map(item => {
+          const step = hasPanelGroups ? getStepByPanelGroup(activeStep) : item;
+          const isActiveItem = hasPanelGroups
+            ? step.panelGroupId === item.id
+            : step.stepNumber === activeStep;
+
+          return (
+            <li
+              key={item.id}
+              style={{
+                display: step.isHidden ? "none" : "block",
+                background: isActiveItem ? "gold" : "lightgray"
+              }}
+              className={isActiveItem ? "highlight" : ""}
+            >
+              {item.label}
+            </li>
+          );
+        })}
       </ol>
     </div>
   );
