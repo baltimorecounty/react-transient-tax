8f10aa3719639f18b4611d8fe113b8f856abd337
martypowell <jpowel25@gmail.com>
2019-12-23 10:33:21 -0500

    added ie support

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import React from "react";
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
 import "react-datepicker/dist/react-datepicker.css";
-import ReactDOM from "react-dom";
-import App from "./App";
+
 import * as serviceWorker from "./serviceWorker";
+
+import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
