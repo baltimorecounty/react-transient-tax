67274183818286748e8659fbbde4e5b7373b8bd0
martypowell <jpowel25@gmail.com>
2019-12-27 08:51:58 -0500

    converted to use formik field

---
+++ src/components/PaymentOptionsField.jsx
@@ -0,0 +1,46 @@
+import { Field } from "formik";
+import { PaymentDirections } from "../common/Constants";
+import { RadioButton } from "../common/RadioButton";
+import React from "react";
+
+const { PaymentLabel, PaymentNote } = PaymentDirections;
+
+const PaymentOptions = ({
+  field: { name }, // { name, value, onChange, onBlur }
+  form: { setFieldValue }, // also values, setXXXX, handleXXXX, dirty, isValid, status, etc.
+  ...props
+}) => {
+  const { filingTypes = {}, onChange = () => {} } = props;
+
+  const handleChange = changeEvent => {
+    const { value } = changeEvent.target;
+    setFieldValue(name, value);
+    onChange(value);
+  };
+
+  return (
+    <React.Fragment>
+      <label htmlFor={name} className="tt_label">
+        {PaymentLabel}
+      </label>
+      {filingTypes.map(({ Id: key, Description: value }) => (
+        <Field
+          key={key}
+          component={RadioButton}
+          name={name}
+          id={`${name}-${key}`}
+          label={value}
+          value={key}
+          onChange={handleChange}
+        />
+      ))}
+      <p>{PaymentNote}</p>
+    </React.Fragment>
+  );
+};
+
+const PaymentOptionsField = props => (
+  <Field component={PaymentOptions} {...props} />
+);
+
+export default PaymentOptionsField;
