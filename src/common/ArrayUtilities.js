6d0d981098b9f87bbc5e92787bef09226028f858
martypowell <jpowel25@gmail.com>
2019-12-30 10:09:47 -0500

    fixed bad param type

--- src/common/ArrayUtilities.js
+++ src/common/ArrayUtilities.js
@@ -2,7 +2,7 @@
  * Adds or updates a list of items
  * @param {Array} items list of objects. Object must contain
  * @param {object} savedItem the new or updated object to be added or updated inside the list of items
- * @param {string} callback A function to execute on each value in the array until the function returns true, indicating that the satisfying element was found
+ * @param {function} callback A function to execute on each value in the array until the function returns true, indicating that the satisfying element was found
  * see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/findIndex
  * @return {Array} new array of with the added or updated item
  */
