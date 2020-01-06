7afeb692e7cd46c91fba3078b8678c3553206ffc
martypowell <jpowel25@gmail.com>
2020-01-06 08:00:33 -0500

    moved method to here, for testing purposes

--- src/common/ErrorUtility.js
+++ src/common/ErrorUtility.js
@@ -1,4 +1,27 @@
 const ErrorPath = error =>
-  `/error/${error.response ? "invalidConfirmation" : "network"} `;
+  `/error/${error.response ? "invalidConfirmation" : "network"}`;
 
-export { ErrorPath };
+/**
+ * Get a heading and message for a given error type
+ * @param {string} errorType type of error
+ */
+const GetError = (errorType = "") => {
+  switch (errorType.toLowerCase()) {
+    case "invalidconfirmation": {
+      return {
+        heading: "Invalid Confirmation Number",
+        message:
+          "You have provided an invalid confirmation number. Please try submitting your Transient Occupancy Tax Return again. "
+      };
+    }
+    default: {
+      return {
+        heading: "System Error",
+        message:
+          "The system has encountered an error. Please wait a few minutes and try again."
+      };
+    }
+  }
+};
+
+export { ErrorPath, GetError };
