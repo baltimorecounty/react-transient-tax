d54b65acedca92aa704811c9bdbb8db8c3934d94
sgurung <sgurung@OIT228469>
2020-08-17 08:28:52 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,69 +1,69 @@
-import "react-datepicker/dist/react-datepicker.css";
-import "./App.scss";
+// import "react-datepicker/dist/react-datepicker.css";
+// import "./App.scss";
 
-import { Redirect, Route, HashRouter as Router } from "react-router-dom";
+// import { Redirect, Route, HashRouter as Router } from "react-router-dom";
 
-import { Config } from "@baltimorecounty/javascript-utilities";
-import ConfirmationPage from "./pages/ConfirmationPage";
-import ErrorPage from "./pages/ErrorPage";
-import { GetError } from "./common/ErrorUtility";
-import { GetQueryParam } from "./common/Routing";
-import MultiPageForm from "./components/MultiPageForm";
-import React from "react";
-import TransientTaxStepList from "./steps/TransientTaxStepList";
+// import { Config } from "@baltimorecounty/javascript-utilities";
+// import ConfirmationPage from "./pages/ConfirmationPage";
+// import ErrorPage from "./pages/ErrorPage";
+// import { GetError } from "./common/ErrorUtility";
+// import { GetQueryParam } from "./common/Routing";
+// import MultiPageForm from "./components/MultiPageForm";
+import React, { Component } from "react";
+// import TransientTaxStepList from "./steps/TransientTaxStepList";
 
-const { setConfig } = Config;
+// const { setConfig } = Config;
 
-const localApiRoot = "//localhost:54727/api";
-const testApiRoot = "http://testservices.baltimorecountymd.gov/api";
-const prodApiRoot = "https://services.baltimorecountymd.gov/api";
+// const localApiRoot = "//localhost:54727/api";
+// const testApiRoot = "http://testservices.baltimorecountymd.gov/api";
+// const prodApiRoot = "https://services.baltimorecountymd.gov/api";
 
-const configValues = {
-  local: {
-    apiRoot: localApiRoot
-  },
-  development: {
-    apiRoot: testApiRoot
-  },
-  staging: {
-    apiRoot: testApiRoot
-  },
-  production: {
-    apiRoot: prodApiRoot
+// const configValues = {
+//   local: {
+//     apiRoot: localApiRoot
+//   },
+//   development: {
+//     apiRoot: testApiRoot
+//   },
+//   staging: {
+//     apiRoot: testApiRoot
+//   },
+//   production: {
+//     apiRoot: prodApiRoot
+//   }
+// };
+
+// setConfig(configValues);
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+        selectedFile: null
+      }
+   
   }
-};
+  onChangeHandler=event=>{
+    this.setState({
+      selectedFile: event.target.files[0],
+      loaded: 0,
+    })
+    console.log(event.target.files[0])
 
-setConfig(configValues);
+}
 
-const App = () => (
-  <div className="tt_app">
-    <Router>
-      <Route exact path="/">
-        <Redirect to="/steps/basic-information" />
-      </Route>
-      <Route
-        exact
-        path="/steps/:stepId"
-        render={props => (
-          <MultiPageForm stepList={TransientTaxStepList} {...props} />
-        )}
-      />
-      <Route
-        exact
-        path="/confirmation/:confirmationNumber"
-        component={ConfirmationPage}
-      />
-      <Route
-        exact
-        path="/error/:errorType"
-        render={({ match = {} }) => {
-          const errorType = GetQueryParam(match, "errorType");
-          const { heading, message } = GetError(errorType);
-          return <ErrorPage heading={heading} message={message} />;
-        }}
-      />
-    </Router>
-  </div>
-);
+onClickHandler = () => {
+  const data = new FormData() 
+  data.append('file', this.state.selectedFile)
+}
+  render() {
+    return (
+      <div className="App">
+      <input type="file" name="file" onChange={this.onChangeHandler}/>
+      <button type="button" class="btn btn-success btn-block" onClick={this.onClickHandler}>Upload</button> 
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import * as serviceWorker from "./serviceWorker";
 import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom";
-
+import "./index.css";
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);
 serviceWorker.unregister();
